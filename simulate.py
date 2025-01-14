96102cd5a39c20ad4c1b1591a8ebd1c489594d4b
Tuckers15 <taschulz@uvm.edu>
2025-01-14 11:07:54 -0500

    creating physics engine with for loop

--- simulate.py
+++ simulate.py
@@ -1,4 +1,13 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0) #optional (commented out for now)
+
+for i in range(1000):
+    print(i)
+    p.stepSimulation()
+    time.sleep(1/60)
+
+
 p.disconnect()
